c8b2e5045b2dfbd0cf078499e2abb83f75ed2536
Anthony Lepore <violacellonyc@gmail.com>
2020-07-29 14:52:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,16 +4,17 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.size == 0 
     puts "The line is currently empty."
-  else
+  
     i = katz_deli.size
     j = i + 1 
     newline = ["The line is currently "]
     while j < i do
+      newline.push(j)
       
       
   
   
+end
   
-  end
   
 end
