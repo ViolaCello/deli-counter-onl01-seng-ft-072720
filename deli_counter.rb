0ffcf0bae4bcbade10ae5d335ea02a3455072efd
Anthony Lepore <violacellonyc@gmail.com>
2020-07-29 16:28:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -31,5 +31,12 @@ end
 def now_serving(katz_deli)
   if katz_deli.length == 0 
     puts "There is nobody waiting to be served!"
+  else
+    a = katz_deli.shift
+    puts "The line is currently serving #{a}."
+  
   end
+  
+  
+  
 end
