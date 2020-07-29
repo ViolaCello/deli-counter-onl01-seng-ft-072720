813bbb24fe4785732c8d70982f1d45055171598c
Anthony Lepore <violacellonyc@gmail.com>
2020-07-29 16:10:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,24 +4,9 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.size == 0 
     puts "The line is currently empty."
-  end
-    i = katz_deli.size
-    j = 1 
-    k = 0 
-    newline = ["The line is currently "]
-    while j < i do
-      newline.push(j)
-      newline[k].concat(". ")
-      newline[k].concat(katz_deli[k])
-      newline[k].concat(" ")
-      
-      
-  j = j + 1 
-  k = k + 1 
-end
-  
-  puts newline.join
-  
+  else
+    
+
 
   
   
