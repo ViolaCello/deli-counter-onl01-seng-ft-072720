4974047ebed7ca84cc59e3177a99db86bcebc6a8
Anthony Lepore <violacellonyc@gmail.com>
2020-07-29 16:17:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,10 +6,15 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     i = 1 
-    puts "The line is currectly: "
+    new_arr = []
     katz_deli.each do |position|
-      puts " #{i}. #{position}"
+     new_array.push(" #{i}. #{position}")
     end
+    a =  "The line is currectly: "
+    b = new_array.join
+    
+    puts a.concat(b)
+    
   end
 
 
